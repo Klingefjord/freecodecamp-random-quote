0c5755ad8da358e91df85ef61e052623addc70c2
Oliver Klingefjord <Oliver@Marias-MacBook-Air.local>
2017-02-09 23:15:08 +0100

    hey

---
+++ js/script.js
@@ -0,0 +1,41 @@
+
+var quoteKey = "9Bnh5taZmUmshhs1LHyWEz52mSb2p1YTlyKjsn3qjYWYWZDiXh";
+var quote;
+var author;
+
+//load initial content
+changer();
+updateTwitter();
+
+//Click on quote button
+$("#quote-btn").click(function(){
+  changer();
+});
+
+//changes quote
+function changeQuote(quote, author) {
+  $("#quote").html('<i>"' + quote + '"</i>');
+  $("#author").html("- " + author);
+}
+
+//sends an api request & calls changeQuote
+function changer(){
+ $.ajax({
+    url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=movies', // The URL to the API. You can get this in the API page of the API you intend to consume
+    type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
+    dataType: 'json',
+    success: function(data) {
+      quote = data.quote;
+      author = data.author;
+      changeQuote(quote, author); //changes quotation
+      updateTwitter(); //updates twitter
+   },
+    beforeSend: function(xhr) {
+    xhr.setRequestHeader("X-Mashape-Authorization", quoteKey); // Enter here your Mashape key
+    }
+  });
+}
+
+function updateTwitter() {
+  $("#twitter-link").attr('href', 'https://twitter.com/intent/tweet?url=[your URL]&text="' + quote + '" - ' + author);
+}
